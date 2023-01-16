1c137fea3326195b13f23299d262aebecf3bc14e
lmoreyra <lmoreyra@proveedor.intercorp.com.pe>
2023-01-15 19:53:35 -0500

    Added exchange rate controller

---
+++ currency-converter-api/src/main/java/pe/interbank/currencyconverterapi/controller/ExchangeRateController.java
@@ -0,0 +1,25 @@
+package pe.interbank.currencyconverterapi.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import pe.interbank.currencyconverterapi.bean.ExchangeAmountRequest;
+import pe.interbank.currencyconverterapi.bean.ExchangeAmountResponse;
+import pe.interbank.currencyconverterapi.service.ExchangeRateService;
+
+@RestController
+@AllArgsConstructor
+public class ExchangeRateController {
+
+    ExchangeRateService exchangeRateService;
+
+    @PostMapping(value = "calculate", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ExchangeAmountResponse calculate(@RequestBody ExchangeAmountRequest exchangeAmountRequest) {
+        return exchangeRateService.calculate(exchangeAmountRequest);
+    }
+
+}
